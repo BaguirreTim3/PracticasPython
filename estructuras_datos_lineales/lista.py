5292ec276b3ee42e29bcd0769a70c59fc1405054
bresar <bremysAguirre>
2022-02-02 15:44:14 -0500

    estructuras_lineales

---
+++ estructuras_datos_lineales/lista.py
@@ -0,0 +1,27 @@
+gatos = ['puka', 'rochy', 'uva', 25]
+frutas = ['melon', 'pera', 'uva', 'fresa']
+
+print(gatos)
+print(frutas)
+
+gatos.append(['mota', 'percy'])
+frutas.append('melocoton')
+
+print(gatos)
+print(frutas)
+
+frutas.sort()
+
+print(frutas)
+
+gatos.pop()
+print(gatos)
+
+frutas.insert(2, 'papaya')
+print(frutas)
+
+frutas.remove('pera')
+print(frutas)
+
+frutas.pop(3)
+print(frutas)
