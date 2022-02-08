b465f548c784fd2c590f17c6f66276a2d5b4f2a1
bresar <bremysAguirre>
2022-02-08 11:06:40 -0500

    select language

---
+++ cursoSelenium/select_language.py
@@ -0,0 +1,49 @@
+from lib2to3.pgen2 import driver
+import unittest
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+
+class LanguajeOptions(unittest.TestCase):
+    
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+        driver = self.driver
+        driver.implicitly_wait(30)
+        driver.maximize_window()
+        driver.get('http://demo-store.seleniumacademy.com')
+        
+        
+        
+    def test_select_languaje(self):
+        exo_options = ['English', 'French', 'German']
+        act_options = []
+        
+        
+        select_language = Select(self.driver.find_element_by_id('select-language'))
+        self.assertEqual(3, len(select_language.options))
+        self.assertEqual('English', select_language.first_selected_option.text)
+        
+        for option in select_language.options:
+            act_options.append(option.text)
+            
+            
+        self.assertListEqual(exo_options, act_options)    
+        select_language.select_by_visible_text('German')
+        
+        self.assertTrue('store=german' in self.driver.current_url)
+        
+        select_language = Select(self.driver.find_element_by_id('select-language'))
+        select_language.select_by_index(0)
+        
+    
+    def tearDown(self):
+        self.driver.implicitly_wait(3)
+        self.driver.quit()
+        
+    
+    
+    
+if __name__ == '__main__':
+    
+    unittest.main(verbosity=2)        
