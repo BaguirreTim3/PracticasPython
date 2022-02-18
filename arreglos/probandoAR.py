7a64aa3c9f87f7c3cc1000f041a6f4c890845667
bremys a <bremysromero85@gmail.com>
2022-02-18 12:58:10 -0500

    prueba de extencion

---
+++ arreglos/probandoAR.py
@@ -0,0 +1,15 @@
+print('esto es una cadena de texto')
+dic = {
+    'nombre': 'bremys',
+    'apellido': 'aguirre',
+        
+}
+
+print(dic)
+
+date = {
+    'name': 'bremys',
+    'surname': 'aguirre'
+}
+
+print(date)
